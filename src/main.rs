ef6f4c023aa924540dfef4934fb0d3d10acb0efd
Nischal Baniya <baniyanisal12@gmail.com>
2025-10-09 21:39:46 +0545

    Initial commit of rust code which cover the concept of mutable reference, lifetime trait, dereference as beginner concept

---
+++ src/main.rs
@@ -0,0 +1,24 @@
+fn main(){
+    //NOTE:
+    let fn_call = passing_reference();
+    println!("accepting ownership pass by fn by returning---- {}", fn_call);
+
+    //NOTE: mutating concept of referncing
+
+    let mut s1 = String::from("First string declared");
+
+    {
+        let s2 = &mut s1;
+        *s2 = String::from("Changed string");
+    }
+
+    let s3 = &s1;
+
+    println!("s3: {}", s3);
+
+}
+
+fn passing_reference() -> String {
+    let string_in_scope = String::from("String declared in passing reference function");
+    string_in_scope
+}
