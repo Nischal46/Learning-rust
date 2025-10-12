283416805d24dc65b8731b41461957010571b0f2
Nischal Baniya <baniyanisal12@gmail.com>
2025-10-12 21:55:15 +0545

    using mod to transfer logic in differnt otheer file and making object implementation of struct method inside of impl and work with lazy git

--- src/vector.rs
+++ src/vector.rs
@@ -10,4 +10,37 @@ pub fn vector_array(arr: [i32; 4]){
     }
 
     println!("At last after meging array in vector in heap---- {:?}", new_vector_Array);
+
+    println!("Calling of fn inside from another module");
+    println!("---------------------------------------------");
+
+
+    let product1 = Product::new("Laptop".to_string(), 65000, "Dell".to_string());
+    println!("Getting detail from constructor function --- {:?}", product1);
+    println!("Getting value from method also------ {:?}", product1.product_details());
+}
+
+#[derive(Debug)]
+struct Product {
+    item: String,
+    price: i32,
+    brand: String
+}
+
+impl Product {
+    // add code here
+    //making as constructor function
+
+    fn new(item: String, price: i32, brand: String) -> Self {
+        Self {
+            item: item.to_string(),
+            price: price,
+            brand: brand.to_string(),
+        }
+    }
+
+    fn product_details(&self) {
+        println!("--------Product Details-----------");
+        println!("Item: {}, Price: {}, Brand: {}", self.item, self.price, self.brand);
+    }
 }
