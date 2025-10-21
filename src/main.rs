061192c90ca4106aea5e8403a94cc17985ffb747
Nischal Baniya <baniyanisal12@gmail.com>
2025-10-21 22:01:23 +0545

    made contact manager app cli with the usage of the rust concept that i had learned through from begging days

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,8 @@
+use crate::user_input::UserStruct;
+use colored::Colorize;
+use console::style;
 use std::io;
+use tabled::Table;
 mod user_input;
 
 fn display_details() {
@@ -11,30 +15,83 @@ fn display_details() {
     )
 }
 
-fn user_menu_choice() -> u8 {
+fn user_menu_choice() -> String {
     let mut user_choice = String::new();
     io::stdin()
         .read_line(&mut user_choice)
         .expect("Error with io modules");
 
-    let response = user_choice.replace("\n", "").parse::<u8>().unwrap();
+    let response = user_choice.replace("\n", "");
     response
 }
 
-fn main() {
-    let contact_array: Vec<user_input::UserStruct> = Vec::new();
-    println!("Welcome to Call Manager App");
+fn app_run_flag(inp: &mut bool) {
+    *inp = false;
+}
 
-    loop {
+fn check_type(input: &String) -> &'static str {
+    if input.parse::<i32>().is_ok() {
+        "integer"
+    } else if input.parse::<f64>().is_ok() {
+        "float"
+    } else {
+        "string"
+    }
+}
+
+fn main() {
+    let mut contact_array: Vec<user_input::UserStruct> = Vec::new();
+    println!(
+        "{}",
+        "\n -- ********** Welcome to Call Manager App ********** --".blue()
+    );
+    let mut app_run = true;
+
+    while app_run {
         display_details();
         let user_choice = user_menu_choice();
 
-        match user_choice {
-            1 => {}
+        let check_type = check_type(&user_choice);
 
-            2 => {}
+        if check_type == "integer" {
+            match user_choice.parse::<i8>() {
+                Ok(1) => {
+                    let user_name = user_input::input_modules::user_name_input_fn();
+                    let user_contact = user_input::input_modules::user_contact_input_fn();
+                    let user_address = user_input::input_modules::user_address_input_fn();
 
-            _ => {}
+                    let contact_details = UserStruct {
+                        name: user_name,
+                        contact: user_contact,
+                        address: user_address,
+                    };
+
+                    contact_details.add();
+                    contact_array.push(contact_details);
+                }
+
+                Ok(2) => {
+                    let result = Table::new(&contact_array);
+                    println!("{}", result);
+                    if contact_array.len() == 0 {
+                        println!("{}", style("Empty data").yellow());
+                    }
+                }
+
+                Ok(3) => {
+                    app_run_flag(&mut app_run);
+                    println!("{}", style("\n Quit App Successfully").green());
+                }
+
+                _ => {
+                    println!(
+                        "{}",
+                        style("\n Invalid input. Please choose appropriate").yellow()
+                    );
+                }
+            }
+        } else {
+            println!("{}", style("\n Please use number as above description").red());
         }
     }
 }
--- src/user_input.rs
+++ src/user_input.rs
@@ -1,24 +1,43 @@
+use console::style;
 use std::io;
+use tabled::Tabled;
 
+use crate::user_input::input_modules::{
+    user_address_input_fn, user_contact_input_fn, user_name_input_fn,
+};
+
+#[derive(Debug, Tabled)]
 pub struct UserStruct {
     pub name: String,
     pub contact: String,
-    pub address: String
+    pub address: String,
+}
+
+impl UserStruct {
+    pub fn add(&self) {
+        println!("{}", style("\n Contact added Successfully").green());
+    }
 }
 
 pub mod input_modules {
     use crate::user_input::user_input;
 
-    pub fn user_name_input_fn() {
-        user_input("username".to_string());
+    pub fn user_name_input_fn() -> String {
+        println!("\nEnter name: ");
+        let response = user_input("username".to_string());
+        response.1
     }
 
-    pub fn user_contact_input_fn() {
-        user_input("usercontact".to_string());
+    pub fn user_contact_input_fn() -> String {
+        println!("\nEnter contact number: ");
+        let response = user_input("usercontact".to_string());
+        response.1
     }
 
-    pub fn user_address_input_fn() {
-        user_input("useraddress".to_string());
+    pub fn user_address_input_fn() -> String {
+        println!("\nEnter address: ");
+        let response = user_input("useraddress".to_string());
+        response.1
     }
 }
 
@@ -29,6 +48,40 @@ pub fn user_input(label: String) -> (String, String) {
         .read_line(&mut user_inp)
         .expect("Input not provided");
 
-    let response = user_inp.replace("\n", "");
+    let mut response = user_inp.replace("\n", "");
+
+    if response.is_empty() {
+        if label == "username".to_string() {
+            println!("Name could not be blank");
+            response = user_name_input_fn();
+        } else if label == "usercontact".to_string() {
+            println!("Contact number could not be blank");
+            response = user_contact_input_fn();
+        } else {
+            response = user_address_input_fn();
+        }
+    }
+
+    if label == "usercontact" {
+        for checknum in response.chars().into_iter() {
+            if !checknum.is_numeric() {
+                println!(
+                    "{}",
+                    style("Contact number couldnot have text. please enter correct number").red()
+                );
+                response = user_contact_input_fn();
+                break;
+            }
+        }
+
+        if response.len() != 10 {
+            println!(
+                "{}",
+                style("\n Please enter contact number of ten digit length").red()
+            );
+            response = user_contact_input_fn();
+        }
+    }
+
     (label, response)
 }
