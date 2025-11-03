fbe1050eca009109c3251292906971a11e301f80
Nischal Baniya <baniyanisal12@gmail.com>
2025-11-03 23:56:13 +0545

    working in for loop and if control statement

--- src/main.rs
+++ src/main.rs
@@ -1,48 +1,13 @@
-// trait Product {
-//     fn product_title(&self) -> &str;
-// }
+fn main() {
+    //loop concept
 
-// #[derive(Debug)]
-// struct Laptop {
-//     title: String
-// }
-
-// impl Product for Laptop {
-//     fn product_title(&self) -> &str {
-//         &self.title
-//     }
-// }
-//reading of trait 
-// fn main() {
-//    //trait is like interface like we define interface in ts
-
-//    let product1 = Laptop {
-//     title: "Dell Laptop".to_string()
-//    };
-
-//    println!("{:#?}", product1.product_title());
-// }
-
-// we can make it reusable in other struct form complex data type
-
-trait check_vat {
-    fn apply_vat(&self) -> &str;
-}
-
-struct Salary{
-    employee_name: String,
-    employee_salary: u16
-}
-
-impl check_vat for Salary {
-    fn apply_vat(&self) -> &str {
-        &self
+    for n in 0..10 {
+        if n % 2 == 0 {
+            println!("{} is even number", n)
+        }
+        else {
+            println!("{} is odd number", n)
+        }
     }
 }
-
-fn main() {
-
-}
-
-
 
