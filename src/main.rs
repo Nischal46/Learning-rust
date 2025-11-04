c80a7ad9c8680aaff0d1f7064b8efb4c9c45cb35
Nischal Baniya <baniyanisal12@gmail.com>
2025-11-04 23:42:52 +0545

    revise of struct impl and trait

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,30 @@
-fn main() {
-    //loop concept
+#[derive(Debug)]
+struct ProductDetails {
+    product_type: String,
+    product_brand: String,
+    is_available: bool
+}
 
-    for n in 0..10 {
-        if n % 2 == 0 {
-            println!("{} is even number", n)
-        }
-        else {
-            println!("{} is odd number", n)
-        }
+trait ProductTrait {
+    fn greet(&self) -> String;
+}
+
+impl ProductTrait for ProductDetails {
+    fn greet(&self) -> String {
+        format!("Congratulations. you had won {:#?}", &self.product_brand)
     }
 }
 
+fn main() {
+    use ProductTrait; // Bring trait into scope
+    
+    let product1 = ProductDetails {
+        product_type: "Laptop".to_string(),
+        product_brand: "DELL".to_string(),
+        is_available: true
+    };
+
+    println!("{:#?}", product1);
+    println!("{:#?}", product1.greet());
+
+}
