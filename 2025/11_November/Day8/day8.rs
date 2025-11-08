25578b62d9e4bbc71b4ce75949936f0fc91095c4
Nischal Baniya <baniyanisal12@gmail.com>
2025-11-08 22:24:41 +0545

    delete cargo file and then daily coding on the basis of day as folder

---
+++ 2025/11_November/Day8/day8.rs
@@ -0,0 +1,28 @@
+
+trait Greet {
+    fn greet(&self) -> String;
+}
+
+#[derive(Debug)]
+struct Customer{
+    name: String,
+    email: String
+}
+
+impl Greet for Customer {
+    fn greet(&self) -> String {
+        let greet = "hello".to_string();
+        greet
+    }
+}
+
+fn main() {
+    let obj = Customer {
+        name: "Nischal".to_string(),
+        email: "nischal@rust.com".to_string()
+    };
+
+    println!("{:#?}", obj);
+    println!("{:#?}", obj.greet());
+
+}
