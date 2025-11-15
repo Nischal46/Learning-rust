6cdaca4e1f7c605069ea59087e23ea258f1f9a43
Nischal Baniya <baniyanisal12@gmail.com>
2025-11-15 23:05:26 +0545

    revise trait with struct impl and used of hashmap

---
+++ 2025/11_November/Day15/main.rs
@@ -0,0 +1,64 @@
+use std::collections::HashMap;
+
+trait Area {
+    fn area(&self) -> String;
+}
+
+#[derive(Debug)]
+struct Rectangle {
+    length: u8,
+    breadth: u8,
+}
+
+#[derive(Debug)]
+struct Triangle {
+    breadth: u8,
+    height: u8,
+}
+
+impl Area for Rectangle {
+    fn area(&self) -> String {
+        let area = &self.length * &self.breadth;
+        let response = format!("Area of rectangle: {} cm^2", area);
+        response
+    }
+}
+
+impl Area for Triangle {
+    fn area(&self) -> String {
+        let area = (&self.breadth / 2) * &self.height;
+        let response = format!("Area of triangle: {} cm^2", area);
+        response
+    }
+}
+
+fn main() {
+    let rectangle1 = Rectangle {
+        length: 3,
+        breadth: 5,
+    };
+
+    let triangle1 = Triangle {
+        breadth: 10,
+        height: 7,
+    };
+
+    println!("Object of Rectangle: {:#?}", rectangle1);
+    println!("Object of Triangle: {:#?}", triangle1);
+
+    println!("{}", rectangle1.area());
+    println!("{}", triangle1.area());
+
+    println!("------------------------------------------------------");
+
+    let mut product_collection = HashMap::new();
+
+    product_collection.insert(
+        "rust".to_string(),
+        "i made iot embedded devices by this".to_string(),
+    );
+
+    product_collection.insert("js".to_string(), "language that teaches me programming".to_string());
+
+    println!("Prinitng of the hash map: {:#?}", product_collection);
+}
