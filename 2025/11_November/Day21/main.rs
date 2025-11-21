3de2cb632c593eb29bc0139f279ed033b131f1e8
Nischal Baniya <baniyanisal12@gmail.com>
2025-11-21 23:40:19 +0545

    revise ownership and borrw reference

--- 2025/11_November/Day21/main.rs
+++ 2025/11_November/Day21/main.rs
@@ -25,4 +25,18 @@ fn main() {
 
     println!("{:#?}", movie1);
     println!("{}", movie1.imdb_rating());
+
+    let data1 = 22;
+    let data2 = 34;
+    moving_ownership(data1);
+    borrow_only(&data2);
+    println!("At first data1 has ownership inside main function scope")
+}
+
+fn moving_ownership(inp: i8){
+    println!("After moving ownership data1 has transferred to moving_ownership and value is {}", inp)
+}
+
+fn borrow_only(inp: &i8){
+    println!("Only borrowing reference {}", inp);
 }
