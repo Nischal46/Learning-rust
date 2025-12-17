f0db181a2ef33daaa7a846331faed9a471d9f270
Nischal Baniya <baniyanisal12@gmail.com>
2025-12-17 17:21:58 +0545

    work with vector datatype and then using std rust library to take input from user

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 mod module_concept;
-
 fn main() {
+
+    let mut vector_store_num:Vec<u8> = Vec::new();
+
+    let mut employee_vector:Vec<module_concept::EmployeeDetails> = Vec::new();
 
     let mut input = String::new();
     println!("Enter some input: ");
@@ -12,4 +15,22 @@ fn main() {
         Ok(num) => println!("You had {}!", num),
         Err(_) => println!("You had entered an invalid number!"),
     }
+
+    for n in 0..5 {
+        let response = module_concept::check_for_ownership();
+        vector_store_num.push(response + n);
+    }
+
+    println!("You had {:?}", vector_store_num);
+
+    print!("-------------------------------------------------------------------------");
+    println!("Details of employee");
+
+    for n in 1..3 {
+        let output = module_concept::gather_details();
+        employee_vector.push(output);
+    }
+
+    println!("There are {} employees", employee_vector.len());
+    println!("stored employees {:#?} ", employee_vector);
 }
--- src/module_concept.rs
+++ src/module_concept.rs
@@ -1,3 +1,32 @@
+use std;
 pub fn fn_call_from_other_module() {
     println!("fn_call_from_other_module");
+}
+
+pub fn check_for_ownership() -> u8 {
+    let num = 23;
+    num
+}
+
+#[derive(Debug)]
+pub struct EmployeeDetails {
+    name: String,
+    email: String,
+}
+
+pub fn gather_details() -> EmployeeDetails {
+    let mut name = String::new();
+    println!("Enter name: ");
+    std::io::stdin().read_line(&mut name);
+
+    let mut email = String::from(name.trim());
+    println!("Enter email: ");
+    std::io::stdin().read_line(&mut email);
+
+    let output = EmployeeDetails{
+        name: name.to_string(),
+        email: email.to_string(),
+    };
+
+    output
 }
