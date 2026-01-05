d37620b2a703eaaf3760b3c62c77a4345f588d30
Nischal Baniya <baniyanisal12@gmail.com>
2026-01-05 23:46:33 +0545

    revising simple concept in rust for daily practice

--- src/main.rs
+++ src/main.rs
@@ -1,45 +1,16 @@
-use std::collections::HashMap;
-use std::thread;
-use std::time::Duration;
-
-struct Cacher<T>
-where
-    T: Fn(u32) -> u32,
-{
-    calculation: T,
-    values: HashMap<u32, u32>,
-}
-
-impl<T> Cacher<T>
-where
-    T: Fn(u32) -> u32,
-{
-    fn new(calculation: T) -> Cacher<T> {
-        Cacher {
-            calculation,
-            values: HashMap::new(),
-        }
-    }
-
-    fn value(&mut self, arg: u32) -> u32 {
-        if let Some(v) = self.values.get(&arg) {
-            *v
-        } else {
-            let v = (self.calculation)(arg);
-            self.values.insert(arg, v);
-            v
-        }
-    }
-}
-
 fn main() {
-    let mut expensive_result = Cacher::new(|num| {
-        println!("calculating slowly...");
-        thread::sleep(Duration::from_secs(2));
-        num
-    });
+    //revising of the anonymous function
 
-    println!("Value: {}", expensive_result.value(40));
-    println!("Value: {}", expensive_result.value(50));
-    println!("Value: {}", expensive_result.value(40)); // cached
+    let x = vec![1, 2, 3, 4, 5];
+
+    let iterVectorData: Vec<_> = x.iter().map(|x| x * 2 ).collect();
+
+    let an_x = |x| x+2;
+
+    let new = an_x(5);
+    println!("{}", new);
+
+    println!("{:#?}", iterVectorData);
+
+    assert_eq!(x, iterVectorData);
 }
