5d3a95103c29796e4464b45dba6fff2cd1440be8
Nischal Baniya <baniyanisal12@gmail.com>
2026-01-13 16:42:13 +0545

    implementing impl block and touch about lifetime concept

--- src/main.rs
+++ src/main.rs
@@ -29,6 +29,18 @@ fn check_citizenship(){
     println!("Logging saved data: -> {:#?}", registered_citizen);
 }
 
+#[derive(Debug)]
+struct Product {
+    title: &'static str,
+    price: i32
+}
+
+impl Product {
+    fn new(title: &'static str, price: i32) -> Self {
+        Self { title, price }
+    }
+}
+
 fn check_mobilenumber(){}
 
 fn check_pan(){}
@@ -40,4 +52,24 @@ fn main() {
     // sending of otp, some fake data would stored in vec data type.
     // suppose all of them are different department and try to communicate with them
     check_citizenship();
+
+    let mut string1 = "nischal".to_string();
+
+    let string2 = "b";
+
+    string1.push('w');
+
+    println!("{}", string1);
+    println!("Can be accessed after merging {}", string2);
+
+    let s3 = string1;
+
+    println!("new string: {}", s3);
+
+    let newObj = Product::new("Dell Black Laptop", 12000);
+
+    println!("Logging of prduct obj: {:#?}", newObj);
+
+    //cannot print because its reference had been moved to new variable
+    // println!("trying to log string1 {}", string1)
 }
