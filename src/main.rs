816c4900cb68c3d9c00a39ff2d430dc8ba08c248
Nischal Baniya <baniyanisal12@gmail.com>
2026-01-22 23:28:07 +0545

    trying to understand synchronous and asynchronous behaviour

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,39 @@
 //trying asynchronous coding approach in rust
 
 fn main() {
-    let heap_store_string = String::from("Hello, Heap!");
+    // let heap_store_string = String::from("Hello, Heap!");
 
-    let clone_string = heap_store_string.clone();
+    // let clone_string = heap_store_string.clone();
 
-    let handle_for_two_seconds = std::thread::spawn( move || {
+    // let handle_for_two_seconds = std::thread::spawn( move || {
+    //     std::thread::sleep(std::time::Duration::from_secs(2));
+    //     accept_string_heap_type(&clone_string);
+    // });
+
+    // println!(
+    //     "Although we pass reference only so we can access it as : {}",
+    //     heap_store_string
+    // );
+
+    // handle_for_two_seconds.join().unwrap();
+
+    //doing asynchronous concept
+
+    println!("This is first");
+
+    //this is synchronaus behhaviour
+    // let _async_for_2sec = std::thread::sleep(std::time::Duration::from_secs(3));
+
+    let handle_for_two_seconds = std::thread::spawn(move || {
         std::thread::sleep(std::time::Duration::from_secs(2));
-        accept_string_heap_type(&clone_string);
+        println!("This is asynchronous behhaviour");
     });
 
-    println!(
-        "Although we pass reference only so we can access it as : {}",
-        heap_store_string
-    );
+    println!("This is blocked for 2 sec");
 
     handle_for_two_seconds.join().unwrap();
 }
 
-fn accept_string_heap_type(s: &String) {
-    println!("{}", s);
-}
+// fn accept_string_heap_type(s: &String) {
+//     println!("{}", s);
+// }
