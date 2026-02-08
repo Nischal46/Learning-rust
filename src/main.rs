4682cc0a4e55b1e7c66016ce165719eee57fc68c
Nischal Baniya <baniyanisal12@gmail.com>
2026-02-08 23:28:08 +0545

    revise ownership and returning type

--- src/main.rs
+++ src/main.rs
@@ -1,76 +1,25 @@
-#[derive(Debug)]
-struct Product<'a>{
-    title: &'a str,
-    brand: &'a str,
-    price: u64,
-    stock_quantity: u8
+fn main() -> Result<(), Box<dyn std::error::Error>>{
+    let data_1 = "Hey developer";
+
+    let data_2 = String::from("Rust rules");
+
+ 
+    passing_ownership_of_string_literals(data_1);
+
+    passing_ownership_of_string_of_heap_concept(data_2);
+
+    println!("Checking if we can access {}", data_1);
+
+    //println!("Checking for especially string that stores as heap: {}", data_2);
+
+    Ok(())
 }
 
-impl<'a> Product<'a> {
-    //associative fn similar to constructor
-    //difference measure by Self
-
-    fn new(title: &'a str, brand: &'a str, price: u64, stock_quantity: u8) -> Self {
-        Self {
-            title: title,
-            brand: brand,
-            price: price,
-            stock_quantity
-        }
-    }
-
-    fn is_expensive_product(&self){
-        if *&self.price > 12000 {
-            println!("{} is expensive product cost Rs {}", &self.title, &self.price);
-        }
-        else {
-            println!("{} is less expensive as Rs {}", &self.title, &self.price);
-        }
-    }
+fn passing_ownership_of_string_literals(data: &str){
+    println!("Logging in function scope -- {}", data);
 }
 
-//NOTE: simply passing of lifetime parameter in function or string slices
-
-fn helper_fn(inp: &str){
-    println!("You had passed this string: {}", inp);
-}
-
-fn main() {
-    let mut product_array: Vec<Product> = Vec::new();
-    println!("This code is written in neovim ide");
-    //product_array.push("Hello");
-    let obj1= Product {
-        title: "Chair",
-        brand: "Well known",
-        price: 11500,
-        stock_quantity: 1
-    };
-
-    let obj2 = Product {
-        title: "Monitor",
-        brand: "Benq",
-        price: 20000,
-        stock_quantity: 1
-    };
-
-    product_array.push(obj1);
-
-    let constructor_object = Product::new("Laptop", "Dell", 64000, 2);
-    constructor_object.is_expensive_product();
-    println!("Logging of the object by constructor function: {:#?}", constructor_object);
-
-    let constructor_object = Product::new("Mouse", "Fantech", 1500, 1);
-    constructor_object.is_expensive_product();
-println!("Object of constructor ---{:#?}", constructor_object);
-    product_array.push(obj2);
-    println!("Logging of the product array: {:#?}", product_array);
-
-    helper_fn("This is only string slices");
-
-    let a = [1,2,3,4,5];
-
-    let slice = &a[0..4];
-
-    println!("logging slices: {:?}", slice);
-
+fn passing_ownership_of_string_of_heap_concept(data: String){
+    println!("Passing of the ownership in thhhis function scope: {}", data);
+    println!("Check whether: {}", data);
 }
