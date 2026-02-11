3a2598b02a274f8f75f1b1e5c450d23ca8e4b289
Nischal Baniya <baniyanisal12@gmail.com>
2026-02-11 23:50:33 +0545

    trying to understand more about lifetime

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,35 @@
-#[derive(Debug)]
-struct Developer<'a> {
-    name: &'a str,
-    postion: &'a str
+fn main(){
+    //concept about lifetime 
+
+    let ptr;
+    let another_ptr;
+
+    {
+        let declare_String: &'static str = "Inside scope";
+        ptr = declare_String; 
+
+        let num : &'static i8 = &10;
+        another_ptr = num;
+        println!("Accessing inside: {}", num);
+        println!("Accessing inside: {}", another_ptr);
+    }
+
+    // println!("Logging declare_String outside: {}", declare_String);
+
+    println!("Accessing outside: {}", ptr);
+    println!("Accessing outside: {}", another_ptr);
+
+    let result = prevent_dangling_reference();
+    println!("Accessing outside: {}", result);
 }
 
-fn main<'a>(){
-    let dev_1 = Developer {
-        name: "Nischal",
-        postion: "Senior Rust Engineer"
-    };
-
-    let x = 5;
-
-    let grap_value;
-
+fn prevent_dangling_reference() -> &'static str{
+    let result;
     {
-        let var_a = &x;
-        println!("Accessing value of a: {}", var_a);
-    }
-
-    {
-        let make_lifetime: &'a i8 = 3;
-        grap_value = &make_lifetime;
-        println!("Inside scope lifetime: {}", make_lifetime);
-    }
-
-    println!("Logging of the object of developer: {:#?}", dev_1);
-    println!("Accessing of lifetime from outside: {}", make_lifetime);
+        // let new_lifetime : &'static str = "i am still active from differnt function";
+        let new_lifetime : &'static str = "i am still active from differnt function dsf";
+        result = new_lifetime;
+        println!("Accessing inside: {}", new_lifetime);
+    } 
+    result
 }
