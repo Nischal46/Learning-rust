0272a13d0fb23c36e77c0d2759ba3a69d8331fb4
Nischal Baniya <baniyanisal12@gmail.com>
2026-03-01 23:11:59 +0545

    revise

--- src/main.rs
+++ src/main.rs
@@ -4,4 +4,8 @@ pub mod modandfunction;
 fn main() {
     datatype::rust_data_type();
     modandfunction::check_user_details("nischal@dev.com".to_owned(), "qwerty");
+
+    for v in 1..12 {
+        println!("{}", v);
+    }
 }
