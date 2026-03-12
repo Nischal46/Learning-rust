1f77add7239ca14224d50d8f509ece8c354facbd
Nischal Baniya <baniyanisal12@gmail.com>
2026-03-12 17:27:40 +0545

    make changes of object through cell for only specific field

--- src/structconcept.rs
+++ src/structconcept.rs
@@ -1,56 +1,15 @@
-trait AreaTrait {
-    fn area(&self) -> u8;
-}
-
-trait ResultTrait {
-    fn result(&self) -> &str;
-}
-
 #[derive(Debug)]
-struct Rectangle {
-    length: u8,
-    breadth: u8,
-}
-
-#[derive(Debug)]
-struct Traingle {
-    breadth: u8,
-    height: u8,
-}
-
-//rust donot directly multiple trait
-
-impl AreaTrait for Rectangle {
-    fn area(&self) -> u8 {
-        2 * (self.breadth + self.length)
-    }
-}
-
-impl ResultTrait for Rectangle {
-    fn result(&self) -> &str {
-        let res = "result found";
-        res
-    }
-}
-
-impl AreaTrait for Traingle {
-    fn area(&self) -> u8 {
-        (self.breadth * self.height) / 2
-    }
+struct Product {
+    title: &'static str,
+    price: std::cell::Cell<u32>,
 }
 
 pub fn struct_concept() {
-    let rec = Rectangle {
-        length: 12,
-        breadth: 15,
+    let obj = Product {
+        title: "Laptop",
+        price: std::cell::Cell::new(23000),
     };
 
-    let trianle_1 = Traingle {
-        breadth: 12,
-        height: 10,
-    };
-
-    println!("Area of triangle: {} cm^2", trianle_1.area());
-
-    println!("Result of area of rectangle: {} cm^2", rec.area());
+    obj.price.set(45000);
+    println!("Logging obj ---- {:?}", obj);
 }
