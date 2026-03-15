9e93679dd2a57ebd691a1a7425e86f0e346f7489
Nischal Baniya <baniyanisal12@gmail.com>
2026-03-15 22:04:57 +0545

    struct with debug trait

--- src/generics.rs
+++ src/generics.rs
@@ -1,51 +1,27 @@
 pub mod generic_concept {
 
-    use std::ops::Add;
-
     #[derive(Debug)]
-    struct Coordinates<T> {
-        x: T,
-        y: T,
+    struct Rectangle<T, S> {
+        length: T,
+        breadth: S,
     }
 
-    #[derive(Debug)]
-    enum Input<T> {
-        Some(T),
-        None,
-    }
-
-    fn sum<T: Add<Output = T>>(inp1: T, inp2: T) -> T {
-        inp1 + inp2
+    impl<T, S> Rectangle<T, S>
+    where
+        T: std::fmt::Debug,
+        S: std::fmt::Debug,
+    {
+        fn describe(&self) {
+            println!("Length: {:?} and breadth: {:?}", self.length, self.breadth);
+        }
     }
 
     pub fn generic_fn() {
-        println!("Generic fn called");
+        let area_rectangle = Rectangle {
+            length: 20,
+            breadth: 30,
+        };
 
-        let coordinate_int_obj = Coordinates { x: 2, y: 3 };
-
-        println!("Logging of coordinate_obj: {:?}", coordinate_int_obj);
-
-        let coordinate_float_obj = Coordinates { x: 1.20, y: 3.30 };
-
-        println!(
-            "Logging of coordinate_float_obj: {:?}",
-            coordinate_float_obj
-        );
-
-        let user_inp = Input::Some("Hello");
-        println!("Logging enum: {:?}", user_inp);
-
-        let user_int_inp = Input::Some(23);
-        println!("Logging enum user_int_inp: {:?}", user_int_inp);
-
-        let user_float_inp = Input::Some(25.45);
-        println!("Logging enum user_float_inp: {:?}", user_float_inp);
-
-        let add_generic_fn = sum(12, 13);
-        println!("using generics sum fn: {}", add_generic_fn);
-
-        //generate error string has refernce
-        // let string_concatinate = sum("nischal", " baniya");
-        // println!("string concatinate: {}", string_concatinate);
+        area_rectangle.describe();
     }
 }
