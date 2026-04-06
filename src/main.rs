0ad7d3efa57371a62bcde7dc63f66cf093f8a077
Nischal Baniya <baniyanisal12@gmail.com>
2026-04-06 23:35:13 +0545

    taking input from user recursively if not valid

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,18 @@
 mod projects;
 
+#[derive(Debug)]
+enum Product {
+    Monitor,
+    Mouse,
+}
+
 fn main() {
     let name = "Nischal";
     println!("Hello rust developer {}", name);
+
+    let choosen_product = Product::Monitor;
+
+    println!("choosen product was: {:?}", choosen_product);
 
     //projects::todo::todo::main_todo();
 
--- src/projects/tictactoe/tictactoe.rs
+++ src/projects/tictactoe/tictactoe.rs
@@ -21,23 +21,59 @@ fn board(ref_pass_array: &mut [[&str; 3]; 3]) {
     //println!("Printing 3d array board -- {:?}", board_3d_array);
 }
 
+fn check_available_or_not(ref_pass_array: &mut [[&str; 3]; 3], player: &str) {
+    if ref_pass_array[0][0] != "" {
+        println!("This position in board is already filled");
+    } else {
+        println!("Empty position. filled now");
+    }
+}
+
+fn take_user_input() -> i32 {
+    println!("Please choose option");
+    let mut choice = String::new();
+    let output;
+    io::stdin()
+        .read_line(&mut choice)
+        .expect("Failed to read stdin");
+
+    match choice.trim().parse::<i32>() {
+        Ok(i) => {
+            output = i;
+            return output;
+        }
+        Err(_) => {
+            println!("Please enter a valid number. Try agian!");
+            take_user_input()
+        }
+    }
+}
+
+enum Player {
+    X,
+    O,
+}
+
 fn init() {
     println!("Game logic start from here --- ");
     let mut board_3d_array: [[&str; 3]; 3] = [["", "", ""], ["", "", ""], ["", "", ""]];
     let mut count = 1;
+    let mut player = Player::X;
 
     while count < 10 {
-        clear_screen();
         inserting_char_in_board(&mut board_3d_array, count);
+        take_user_input();
+        //clear_screen();
         board(&mut board_3d_array);
         count += 1;
     }
 }
 
-fn inserting_char_in_board(arr_3d: &mut [[&str; 3]; 3], inp: i32) {
+fn inserting_char_in_board(mut arr_3d: &mut [[&str; 3]; 3], inp: i32) {
     match inp {
         1 => {
             arr_3d[0][0] = "X";
+            check_available_or_not(&mut arr_3d, "X");
         }
         2 => arr_3d[0][1] = "0",
         3 => arr_3d[0][2] = "X",
