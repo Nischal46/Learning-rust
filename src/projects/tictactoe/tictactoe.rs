bc8a3df1ce0ae5ffff2f17cc3b1eb8de09b9800d
Nischal Baniya <baniyanisal12@gmail.com>
2026-04-08 23:47:26 +0545

    changing of array board from &str to char and ready for further development

--- src/projects/tictactoe/tictactoe.rs
+++ src/projects/tictactoe/tictactoe.rs
@@ -11,7 +11,7 @@ fn clear_screen() {
     io::stdout().flush().unwrap();
 }
 
-fn board(ref_pass_array: &mut [[&str; 3]; 3]) {
+fn board(ref_pass_array: &mut [[char; 3]; 3]) {
     println!("This is board..");
     println!("-----------------");
 
@@ -21,8 +21,13 @@ fn board(ref_pass_array: &mut [[&str; 3]; 3]) {
     //println!("Printing 3d array board -- {:?}", board_3d_array);
 }
 
-fn check_available_or_not(ref_pass_array: &mut [[&str; 3]; 3], player: &str) {
-    if ref_pass_array[0][0] != "" {
+fn check_available_or_not(
+    ref_pass_array: &mut [[char; 3]; 3],
+    row: i32,
+    column: i32,
+    player: char,
+) {
+    if ref_pass_array[0][0] != ' ' {
         println!("This position in board is already filled");
     } else {
         println!("Empty position. filled now");
@@ -56,33 +61,33 @@ enum Player {
 
 fn init() {
     println!("Game logic start from here --- ");
-    let mut board_3d_array: [[&str; 3]; 3] = [["", "", ""], ["", "", ""], ["", "", ""]];
+    let mut board_3d_array: [[char; 3]; 3] = [[' ', ' ', ' '], [' ', ' ', ' '], [' ', ' ', ' ']];
     let mut count = 1;
     let mut player = Player::X;
 
     while count < 10 {
-        inserting_char_in_board(&mut board_3d_array, count);
-        take_user_input();
+        let user_choice = take_user_input();
+        inserting_char_in_board(&mut board_3d_array, user_choice, player);
         //clear_screen();
         board(&mut board_3d_array);
         count += 1;
     }
 }
 
-fn inserting_char_in_board(mut arr_3d: &mut [[&str; 3]; 3], inp: i32) {
+fn inserting_char_in_board(mut arr_3d: &mut [[char; 3]; 3], inp: i32, player: Player) {
     match inp {
         1 => {
-            arr_3d[0][0] = "X";
-            check_available_or_not(&mut arr_3d, "X");
+            arr_3d[0][0] = 'X';
+            check_available_or_not(&mut arr_3d, 0, 0, 'X');
         }
-        2 => arr_3d[0][1] = "0",
-        3 => arr_3d[0][2] = "X",
-        4 => arr_3d[1][0] = "0",
-        5 => arr_3d[1][1] = "X",
-        6 => arr_3d[1][2] = "0",
-        7 => arr_3d[2][0] = "X",
-        8 => arr_3d[2][1] = "0",
-        9 => arr_3d[2][2] = "X",
+        2 => arr_3d[0][1] = '0',
+        3 => arr_3d[0][2] = 'X',
+        4 => arr_3d[1][0] = '0',
+        5 => arr_3d[1][1] = 'X',
+        6 => arr_3d[1][2] = '0',
+        7 => arr_3d[2][0] = 'X',
+        8 => arr_3d[2][1] = '0',
+        9 => arr_3d[2][2] = 'X',
         _ => println!("Default option choose"),
     }
 }
