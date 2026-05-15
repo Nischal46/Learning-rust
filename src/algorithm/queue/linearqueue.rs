62a799d774c185a612193d598b5b2f9c8344dd7c
Nischal Baniya <baniyanisal12@gmail.com>
2026-05-15 23:25:24 +0545

    work on queue

--- src/algorithm/queue/linearqueue.rs
+++ src/algorithm/queue/linearqueue.rs
@@ -1,43 +1,75 @@
 #[derive(Debug)]
 struct Queue {
-    data: [i32; 8],
+    data: Vec<i32>, // Changed from array to Vec for dynamic sizing
     front: usize,
-    back: usize,
+    rear: usize,
     capacity: usize,
 }
 
 impl Queue {
-    // NOTE: like constructor
-    fn new() -> Self {
+    fn new(capacity: i32) -> Self {
+        let capacity_usize = capacity as usize;
         Queue {
-            data: [0; 8],
+            data: vec![0; capacity_usize], // Create vector with initial capacity
             front: 0,
-            back: 0,
-            capacity: 0,
+            rear: 0,
+            capacity: capacity_usize,
         }
     }
 
-    fn enqueue(&mut self, item: i32) {
-        self.data.push(item);
+    fn enqueue(&mut self, elem: i32) -> bool {
+        if self.is_full() {
+            println!("Queue is full! Cannot enqueue {}", elem);
+            return false;
+        }
+
+        self.data[self.rear] = elem;
+        self.rear = (self.rear + 1) % self.capacity;
+        true
     }
 
-    fn dequeue(&mut self) {
-        self.data.pop();
+    fn dequeue(&mut self) -> Option<i32> {
+        if self.is_empty() {
+            println!("Queue is empty! Cannot dequeue");
+            return None;
+        }
+
+        let value = self.data[self.front];
+        self.front = (self.front + 1) % self.capacity;
+        Some(value)
     }
 
     fn is_empty(&self) -> bool {
-        self.data.is_empty()
+        self.front == self.rear
+    }
+
+    fn is_full(&self) -> bool {
+        (self.rear + 1) % self.capacity == self.front
+    }
+
+    fn peek(&self) -> Option<&i32> {
+        if self.is_empty() {
+            None
+        } else {
+            Some(&self.data[self.front])
+        }
     }
 }
 
 pub fn init() {
     println!("This is for linear queue concept");
 
-    let mut queue_object = Queue::new();
-    queue_object.enqueue(23);
-    queue_object.enqueue(45);
-    queue_object.enqueue(56);
-    queue_object.dequeue();
+    let mut queue_concept = Queue::new(4);
+    queue_concept.enqueue(23);
+    queue_concept.enqueue(34);
+    queue_concept.enqueue(45);
 
-    println!("Logging of the queue object ------ {:?}", queue_object);
+    println!("After enqueuing: {:?}", queue_concept);
+
+    // Demonstrate dequeue
+    if let Some(value) = queue_concept.dequeue() {
+        println!("Dequeued: {}", value);
+    }
+
+    println!("After dequeuing: {:?}", queue_concept);
 }
