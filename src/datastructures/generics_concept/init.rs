1fbac1f1b07509e6bb2b70bb9a8b166c82d481bb
Nischal Baniya <baniyanisal12@gmail.com>
2026-05-27 22:54:50 +0545

    generics and uses of default Display trait to log

--- src/datastructures/generics_concept/init.rs
+++ src/datastructures/generics_concept/init.rs
@@ -1,33 +1,44 @@
-struct Point<T> {
+//NOTE: using of Generics is to reduce code and make reusable
+use std::{
+    fmt::{Display, Formatter},
+    ops::Add,
+};
+// to deal with int and float
+
+//#[derive(Debug)]
+struct Coordinates<T> {
     x: T,
     y: T,
 }
 
-#[derive(Debug)]
-enum Expect_num<T> {
-    Some(T),
-    None,
+// NOTE: we defined std::fmt::Display instead of Debug rait from derive
+impl<T: Display> Display for Coordinates<T> {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "({}, {})", self.x, self.y)
+    }
+}
+
+impl<T: Add<Output = T> + Copy> Coordinates<T> {
+    fn sum(&self) -> T {
+        self.x + self.y
+    }
+}
+
+impl<T: Display> Coordinates<T> {
+    fn details(&self) {
+        println!("only logging first coordinates: {}", self.x)
+    }
 }
 
 pub fn init() {
-    println!("This is the concept of generics");
+    println!("---------------------Generics concept----------------------");
+    let point_int = Coordinates { x: 2, y: 3 };
 
-    let integer_generics = Point { x: 1, y: 4 };
-    let float_generics = Point { x: 1.5, y: 2.5 };
+    println!("Printing of int Coordinates ----- {}", point_int);
+    println!("Adding of the sum of point: {}", point_int.sum());
 
-    println!(
-        "Logging of the integer x: {}, y: {}",
-        integer_generics.x, integer_generics.y
-    );
-
-    println!(
-        "Logging of the float x: {}, y: {}",
-        float_generics.x, float_generics.y
-    );
-
-    let guessing_num = Expect_num::Some(34);
-    println!("Guessing the num: {:?}", guessing_num);
-
-    let guess_float = Expect_num::Some(23.34);
-    println!("Guessing the float: {:?}", guess_float);
+    let point_float = Coordinates { x: 1.2, y: 2.3 };
+    println!("Printing of float coordinated ----- {}", point_float);
+    println!("Printing the sum of float point: {}", point_float.sum());
+    point_float.details();
 }
