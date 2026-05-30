c239ad00ce8cd8c65b1699e12aa83bb9a6701329
Nischal Baniya <baniyanisal12@gmail.com>
2026-05-30 23:39:20 +0545

    uses of struct trait impl block

--- src/algorithm/queue/linearqueue.rs
+++ src/algorithm/queue/linearqueue.rs
@@ -1,64 +1,31 @@
 pub fn init() {
-    println!("--------------Single ended queue-------------");
-    let mut queue_first_batch = Queue::new(3);
-    queue_first_batch.add(23);
-    queue_first_batch.add(56);
-    queue_first_batch.add(87);
-    queue_first_batch.remove();
-
-    println!(
-        "Logging of single ended queue -------- {:?}",
-        queue_first_batch
-    );
+    println!("This is the concept of the linear queue");
 }
 
-#[derive(Debug)]
-struct Queue {
-    data: Vec<Option<i32>>,
+struct LinearQueue {
+    data: Vec<Option<i8>>,
     front: usize,
-    rear: usize,
+    back: usize,
     capacity: usize,
     count: usize,
 }
 
-impl Queue {
-    fn new(inp: i32) -> Self {
-        let alloc_size = inp as usize;
-        Self {
-            data: vec![None; alloc_size],
+pub trait CheckMargin {
+    fn new(alloc_Size: i8) -> Self;
+    fn check_input_margin(inp: i8);
+}
+
+impl CheckMargin for LinearQueue {
+    fn new(alloc_size: i8) -> Self {
+        let size_determined = alloc_size as usize;
+        LinearQueue {
+            data: vec![None; size_determined],
             front: 0,
-            rear: 0,
-            capacity: alloc_size,
+            back: 0,
+            capacity: size_determined,
             count: 0,
         }
     }
 
-    fn is_full(&self) -> bool {
-        self.count == self.capacity
-    }
-
-    fn add(&mut self, elem: i32) {
-        if self.is_full() {
-            println!("Queue is empty for today.");
-            return;
-        }
-
-        self.data[self.rear] = Some(elem);
-        self.rear += 1;
-        self.count += 1;
-    }
-
-    fn is_empty(&self) -> bool {
-        self.count == 0
-    }
-
-    fn remove(&mut self) {
-        if self.is_empty() {
-            println!("Queue is empty .....");
-            return;
-        }
-
-        self.data[self.front] = None;
-        self.front += 1;
-    }
+    fn check_input_margin(inp: i8) {}
 }
