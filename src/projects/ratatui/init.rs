0f0f877733fec038acfee1fb3f570e633a13e328
Nischal Baniya <baniyanisal12@gmail.com>
2026-06-04 17:19:43 +0545

    implementing block concept and render in frame and also text in ratatui

--- src/projects/ratatui/init.rs
+++ src/projects/ratatui/init.rs
@@ -1,13 +1,15 @@
-use ratatui::Frame;
-use ratatui::layout::{Constraint, Layout, Rect};
-use ratatui::style::{Style, Stylize};
-use ratatui::text::{Line, Span};
-use ratatui::widgets::{Bar, BarChart, Block, BorderType};
-
 use color_eyre::Result;
+use color_eyre::owo_colors::colors::Blue;
 use crossterm::event;
+use ratatui::Frame;
+use ratatui::layout::{Alignment, Constraint, Layout, Rect};
+use ratatui::style::{Color, Style, Stylize};
+use ratatui::text::{Line, Span};
+use ratatui::widgets::{Block, BorderType, Paragraph};
 
 pub fn init() {
+    println!("This is the concept of ratatui....");
+
     color_eyre::install();
     ratatui::run(|terminal| {
         loop {
@@ -18,26 +20,43 @@ pub fn init() {
 
 fn render(frame: &mut Frame) {
     let vertical = Layout::vertical([Constraint::Length(1), Constraint::Fill(1)]).spacing(1);
-    let horizontal = Layout::horizontal([Constraint::Length(28), Constraint::Fill((1))]).spacing(1);
+    let horizontal = Layout::horizontal([Constraint::Percentage(33); 3]).spacing(1);
+
     let [top, main] = frame.area().layout(&vertical);
-    let [left, right] = main.layout(&horizontal);
+    let [left, middle, right] = main.layout(&horizontal);
 
     let title = Line::from_iter([
-        Span::from("Nischal's Barchart\n").bold(),
-        Span::from("Press q to Quit"),
+        Span::from("Block Create\n").bold(),
+        Span::from(" (Press q or Esc to break) "),
     ]);
 
     frame.render_widget(title.centered(), top);
-    render_vertical_barchart(frame, left);
+
+    render_left_block(frame, left);
+    render_middle_block(frame, middle);
+
+    render_paragraph(frame, middle);
 }
 
-fn render_vertical_barchart(frame: &mut Frame, area: Rect) {
-    let bars = vec![
-        Bar::with_label("Red", 30).red(),
-        Bar::with_label("Blue", 20).blue(),
-        Bar::with_label("Green", 15).green(),
-        Bar::with_label("Yellow", 10).yellow(),
-    ];
-    let chart = BarChart::vertical(bars).bar_width(6);
-    frame.render_widget(chart, area);
+fn render_left_block(frame: &mut Frame, area: Rect) {
+    let block = Block::bordered()
+        .style(Style::new().on_magenta())
+        .title("Left Block");
+    frame.render_widget(block, area);
+}
+
+fn render_middle_block(frame: &mut Frame, area: Rect) {
+    let block = Block::bordered()
+        .style(Style::new().blue().on_black().bold().italic())
+        .title("Middle Block");
+    frame.render_widget(block, area);
+}
+
+fn render_paragraph(frame: &mut Frame, area: Rect) {
+    let text = "\nI am Nischal Baniya";
+    let display_text = Paragraph::new(text)
+        .style(Color::White)
+        .alignment(Alignment::Center);
+
+    frame.render_widget(display_text, area);
 }
