80456311e9826cdccf4416bca395f69a66f565ed
Nischal Baniya <baniyanisal12@gmail.com>
2026-06-09 23:51:58 +0545

    work on progress of enum and match

--- src/projects/guitartuner/start.rs
+++ src/projects/guitartuner/start.rs
@@ -1,48 +1,28 @@
-use std::io::{Error, ErrorKind};
-use std::thread;
-use std::time::Duration;
+use std::io::Error;
 
-fn load_config(inp: u8) -> Result<String, std::io::Error> {
-    if inp == 2 {
-        println!("Great! Exact number found.");
+enum GuitarString {
+    E2,
+    A2,
+    D2,
+    G2,
+    B2,
+    E4,
+}
+
+fn play(tune: GuitarString) -> Result<String, Error> {
+    if tune.into() == "E2" {
+        println!("You had pressed E2 string");
+        Ok(String::from("Tunning"))
     } else {
-        // We can manually create an error if the input isn't what we want
-        return Err(Error::new(ErrorKind::InvalidInput, "Number was not 2"));
+        return Err(Error::new(std::io::ErrorKind::NotFound, "not found"));
     }
-
-    // Example: Attempting to read a file that might not exist
-    // The '?' operator will return the error automatically if this fails
-    println!("Sleeping main thread for 3 sec.........");
-    thread::sleep(Duration::from_secs(3));
-    let response = std::fs::read_to_string("afile2.txt")?;
-
-    print!("log just below error stack after 3 sec...........");
-
-    // If we reach here, everything succeeded!
-    Ok(response)
 }
 
 pub fn init() {
-    // Handling the Result from load_config using a match statement
-    match load_config(2) {
-        Ok(data) => println!("Config loaded successfully: {}", data),
-        Err(e) => eprintln!("Failed to load config: {}", e),
-    }
+    println!("Guitar tuner ........");
 
-    println!("This is my project");
-    match login("admin") {
-        Ok(data) => println!("{}", data),
-        Err(e) => println!("Error occured: {}", e),
-    }
-}
-
-fn login<'a>(role: &'a str) -> Result<&'a str, Error> {
-    if role == "admin" {
-        Ok("Login successfully. accessing admin portal")
-    } else {
-        return Err(Error::new(
-            ErrorKind::PermissionDenied,
-            "role not permitted granted",
-        ));
+    match play(GuitarString::E2) {
+        Ok(data) => println!("Success"),
+        Err(e) => println!("Error caught-----", e),
     }
 }
