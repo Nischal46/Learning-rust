73e67ffe41e7bdd6a837ae05dc27126b6ced7e40
Nischal Baniya <baniyanisal12@gmail.com>
2026-06-10 22:33:42 +0545

    linear queue declared struct and impl

--- src/algorithm/queue/linearqueue.rs
+++ src/algorithm/queue/linearqueue.rs
@@ -1,24 +1,25 @@
-use std::any::type_name;
+//NOTE: Linear queue concept
 
-#[derive(Debug, Clone, Copy)]
-struct Line<'a> {
-    name: &'a str,
-    purpose: &'a str,
+#[derive(Debug)]
+struct Task<'a> {
+    id: u8,
+    title: &'a str,
+    is_complete: bool,
 }
 
 #[derive(Debug)]
-struct LinearQueue<'a> {
-    data: Vec<Option<Line<'a>>>,
+struct Queue<'a> {
+    data: Vec<Option<Task<'a>>>,
     front: usize,
     rear: usize,
     capacity: usize,
     count: usize,
 }
 
-impl<'a> LinearQueue<'a> {
-    fn new(inp: i32) -> Self {
-        let alloc_size = inp as usize;
-        LinearQueue {
+impl<'a> Queue<'a> {
+    fn new(determine_size: u8) -> Self {
+        let alloc_size = determine_size as usize;
+        Queue {
             data: vec![None; alloc_size],
             front: 0,
             rear: 0,
@@ -26,70 +27,10 @@ impl<'a> LinearQueue<'a> {
             count: 0,
         }
     }
-
-    fn is_full(&self) -> bool {
-        if self.capacity == self.count {
-            return true;
-        }
-
-        return false;
-    }
-
-    fn add_rear(&mut self, data: Line<'a>) {
-        if self.is_full() {
-            println!("Queue is full.......");
-            return;
-        }
-
-        self.data[self.rear] = Some(data);
-        self.rear += 1;
-        self.count += 1;
-    }
-
-    fn is_empty(&self) -> bool {
-        if self.count == 0 {
-            return true;
-        }
-        return false;
-    }
-
-    fn remove_front(&mut self) {
-        if self.is_empty() {
-            println!("Right now queue is empty......");
-            return;
-        }
-
-        self.data[self.front] = None;
-        self.front += 1;
-    }
-}
-
-fn print_type_of<T>(_: T) {
-    println!("{}", type_name::<T>());
 }
 
 pub fn init() {
-    println!("--------Concept of Linear queue-----------");
+    println!("Linear Queue concept................");
 
-    let mut queue_concept = LinearQueue::new(2);
-    queue_concept.remove_front();
-    queue_concept.add_rear(Line {
-        name: "Nischal Baniya",
-        purpose: "Embedded Rust",
-    });
-
-    queue_concept.add_rear(Line {
-        name: "Rust dev - Nischal Baniya",
-        purpose: "Security",
-    });
-
-    queue_concept.add_rear(Line {
-        name: "Baniya dev",
-        purpose: "Game dev",
-    });
-    println!("Logging of the linear queue ------- {:?}", queue_concept);
-
-    print_type_of(&queue_concept);
-    print_type_of("code daily");
-    print_type_of(String::from("Lovee to code"));
+    let queue_dec = Queue::new(3);
 }
--- src/projects/guitartuner/start.rs
+++ src/projects/guitartuner/start.rs
@@ -1,28 +1,3 @@
-use std::io::Error;
-
-enum GuitarString {
-    E2,
-    A2,
-    D2,
-    G2,
-    B2,
-    E4,
-}
-
-fn play(tune: GuitarString) -> Result<String, Error> {
-    if tune.into() == "E2" {
-        println!("You had pressed E2 string");
-        Ok(String::from("Tunning"))
-    } else {
-        return Err(Error::new(std::io::ErrorKind::NotFound, "not found"));
-    }
-}
-
 pub fn init() {
-    println!("Guitar tuner ........");
-
-    match play(GuitarString::E2) {
-        Ok(data) => println!("Success"),
-        Err(e) => println!("Error caught-----", e),
-    }
+    println!("Guitar tuner");
 }
