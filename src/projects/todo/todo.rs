ea417b7b4b07c8352d04dbdba7fd06f253aa34bb
Nischal Baniya <baniyanisal12@gmail.com>
2026-06-16 17:51:15 +0545

    mplementng todo by linear queue concept with terminal ui ratatui

--- src/projects/todo/todo.rs
+++ src/projects/todo/todo.rs
@@ -1,91 +1,90 @@
 use std::io;
 
+use crossterm::{
+    event::EnableMouseCapture,
+    execute,
+    terminal::{EnterAlternateScreen, enable_raw_mode},
+};
+use ratatui::{Terminal, prelude::CrosstermBackend};
+
+#[derive(Debug, Clone)]
+struct UserDetails<'a> {
+    name_input: &'a str,
+    email_input: &'a str,
+    contact: &'a str,
+}
+
 #[derive(Debug)]
-struct TodoList {
-    id: u16,
-    task: Box<str>,
-    completed: bool,
+struct LinearQueue<'a> {
+    data: Vec<Option<UserDetails<'a>>>,
+    count: usize,
+    front: usize,
+    rear: usize,
+    capacity: usize,
 }
 
-pub fn main_todo() {
-    let mut todo_list: Vec<TodoList> = vec![];
-
-    fn init(todo_list: &mut Vec<TodoList>) {
-        loop {
-            todo_menu();
-
-            let user_choose_input = user_input_menu();
-
-            match user_choose_input {
-                1 => {
-                    let res = user_task_input();
-                    let count = todo_list.len();
-
-                    let task_details = TodoList {
-                        id: count as u16 + 1,
-                        task: res.into_boxed_str(),
-                        completed: false,
-                    };
-
-                    todo_list.push(task_details);
-                }
-
-                2 => {
-                    println!("Reading the list of task ----");
-                    println!("{:?}", todo_list);
-                }
-
-                3 => {
-                    for n in todo_list.into_iter() {
-                        if n.id == 1 {
-                            n.completed = true;
-                        }
-                    }
-                }
-
-                5 => {
-                    break;
-                }
-
-                _ => println!("J paye tei"),
-            }
+impl<'a> LinearQueue<'a> {
+    fn new(user_inp_size: u8) -> Self {
+        let alloc_size = user_inp_size as usize;
+        LinearQueue {
+            data: vec![None; alloc_size],
+            front: 0,
+            rear: 0,
+            count: 0,
+            capacity: alloc_size,
         }
     }
 
-    init(&mut todo_list);
-}
-
-fn todo_menu() {
-    println!("--------------Todo App--------------");
-    println!("-> Press 1 to Add task");
-    println!("-> Press 2 to view task");
-    println!("-> Press 3 as mark done for completed task");
-    println!("-> Press 4 to store in file");
-    println!("-> Press 5 to quit app");
-}
-
-fn user_input_menu() -> i16 {
-    let mut choice = String::new();
-    loop {
-        println!("Please enter a choice: ");
-        io::stdin()
-            .read_line(&mut choice)
-            .expect("Failed to read user input");
-
-        match choice.trim().parse::<i16>() {
-            Ok(num) => return num,
-            Err(_) => println!("Invalid number. please input number"),
+    fn is_full(&self) -> bool {
+        if self.capacity == self.rear {
+            return true;
+        } else {
+            return false;
         }
+    }
+
+    fn is_empty(&self) -> bool {
+        if self.count == 0 {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    fn add_user_in_queue(&mut self, data: UserDetails<'a>) {
+        if self.is_full() {
+            println!("Queue is full............");
+            return;
+        }
+
+        self.data[self.rear] = Some(data);
+        self.rear += 1;
+        self.count += 1;
+    }
+
+    fn remove_user_from_queue(&mut self) {
+        if self.is_empty() {
+            println!("Queue is empty..........");
+            return;
+        }
+
+        self.data[self.front] = None;
+        self.front += 1;
     }
 }
 
-fn user_task_input() -> String {
-    let mut task = String::new();
-    io::stdin()
-        .read_line(&mut task)
-        .expect("failed to read user input");
+pub fn main_todo() -> Result<()> {
+    // NOTE: This is for seting up of terminal
+    enable_raw_mode();
 
-    println!("You have type task as: {}", task);
+    let mut stdot = io::stdout();
 
-    task
+    // NOTE: to capture mouse clicke event
+
+    execute!(stdot, EnterAlternateScreen, EnableMouseCapture)?;
+
+    let backend = CrosstermBackend::new(stdot);
+
+    let mut terminal = Terminal::new(backend)?;
 }
+
