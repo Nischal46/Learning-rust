da7c17c6888710e3e73f23fc9f5ec6877b0a7d20
Nischal Baniya <baniyanisal12@gmail.com>
2026-06-20 20:29:47 +0545

    working in thread concept how it works more than previous learning. simply make like async behaviour from spawn

--- src/datastructures/thread_concept/init.rs
+++ src/datastructures/thread_concept/init.rs
@@ -1,20 +1,40 @@
 use std::{thread, time::Duration};
 
 pub fn init() {
-    //dissecting of the thread concept
+    //NOTE: Concept about the thread
 
-    println!("Thread concept in details");
+    let v = vec!["gitlab", "github", "git"];
 
-    println!("Thread blocks for 3 sec");
-    thread::sleep(Duration::from_secs(3));
-
-    let async_one_thread = thread::spawn(|| {
-        thread::sleep(Duration::from_secs(1));
-        println!("sleep for 1 sec.........");
+    let sub_thread_one = thread::spawn(|| {
+        for i in 1..10 {
+            println!("-> Running --secondary thread-- {} times", i);
+            thread::sleep(Duration::from_millis(1));
+        }
     });
 
-    println!("Easy thread concept knowledge");
+    let sub_thread_two = thread::spawn(|| {
+        for i in 1..20 {
+            println!("-> Running --tertiary thread-- {} times", i);
+            thread::sleep(Duration::from_millis(2));
+        }
+    });
 
-    async_one_thread.join().expect("Failed failed");
+    let transfering_vec_closure = thread::spawn(move || {
+        println!("Heres a vector value expecting -- {:?}.", v);
+    });
+
+    // NOTE: got error as v already move to closure in new thread
+    //println!("Trying to get v after ownership moving to closure {:?}", v);
+
+    sub_thread_one.join().expect("Error occur while spawning");
+    for i in 1..=5 {
+        println!("-> Running --main thread-- {} times", i);
+        thread::sleep(Duration::from_millis(1));
+    }
+
+    sub_thread_two.join().expect("Error in thread two");
+    transfering_vec_closure
+        .join()
+        .expect("Error on passing ownership");
 }
 
