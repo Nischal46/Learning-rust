e84125f70f91907bf76a537485eafb7735d53c1c
Nischal46 <baniyanisal12@gmail.com>
2026-06-29 23:26:55 +0545

    mimum revise

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,28 @@
-use std::{ops::AddAssign, sync::Mutex, thread, time::Duration};
-
-fn main() {
-    println!("Working in closure fn.........");
-
-    let mutex_var = Mutex::new(0u16);
-
-    let fn_call = || {
-        let mut mut_ref = mutex_var.lock().unwrap();
-        for i in 1..3 {
-            println!("Closure fn runs... {} times", i);
-            mut_ref.add_assign(i);
-            thread::sleep(Duration::from_secs(3));
-        }
-
-        println!("Printing value of mutex {:?}", mut_ref);
-    };
-
-    let fn_call2 = || {
-        println!("waitinf for closure 1 to finish");
-        let mut another_mut_ref = mutex_var.lock().unwrap();
-        println!("Closure fn 2");
-
-        for i in 1..8 {
-            println!("Closure fn 2 runs .... {} times", i);
-            another_mut_ref.add_assign(i);
-        }
-
-        println!("Priniting value of mutex .... {:?}", another_mut_ref);
-    };
-
-    fn_call();
-    fn_call2();
+enum Continent {
+    Asia,
+    Africa,
+    Europe,
+    SouthAmerica,
+    NorthAmerica,
 }
 
+#[derive(Debug)]
+struct Country<'a> {
+    country: &'a str,
+    continent: &'a str,
+}
+
+#[derive(Debug)]
+struct Team<'a> {
+    data: Vec<Country<'a>>,
+    count: usize,
+}
+
+fn main() {
+    let country_one = Country {
+        country: "Japan",
+        continent: "Asia",
+    };
+
+    println!("{:?}", country_one);
+}
