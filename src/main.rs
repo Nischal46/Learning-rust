9e4b152f422e5559784f075ef32cdcddb20e4280
Nischal46 <baniyanisal12@gmail.com>
2026-07-02 23:27:14 +0545

    make changes

--- src/main.rs
+++ src/main.rs
@@ -25,6 +25,24 @@ fn main (){
         drop(data);
     });
 
+        let thread_one = thread::spawn(move || {
+        thread::sleep(Duration::from_secs(3));
+        let mut data = mutex_one.lock().expect("Error in thread one");
+        println!("This is thread one");
+        *data = 45;
+        println!("Logging of mutex guard val in thread one {:?}", data);
+    });
+
+    let mutex_two = Arc::clone(&mutex_data);
+    let thread_two = thread::spawn(move || {
+        let mut data = mutex_two.lock().expect("error in expecting mutex data in thread two");
+        data.add_assign(33);
+        println!("This is thread two");
+        println!("Logging of the mutex val {:?}", data);
+        drop(data);
+    });
+
+
     thread_one.join().unwrap();
     thread_two.join().unwrap();
 
