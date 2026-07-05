65c19e545784b3686e4736a0121d856d62cff14c
Nischal46 <baniyanisal12@gmail.com>
2026-07-05 21:46:26 +0545

    implement logic added of taking input from user and loop and dereference concept

--- src/projects/tictactoe.rs
+++ src/projects/tictactoe.rs
@@ -1,10 +1,4 @@
-use std::{thread, time::Duration};
-
-#[derive(Debug)]
-enum Player {
-    X,
-    Y
-}
+use std::{io, thread, time::Duration};
 
 fn board_display(board: &[[char; 3]]) {
     println!("Initializing Board...");
@@ -13,14 +7,40 @@ fn board_display(board: &[[char; 3]]) {
     }
 }
 
+fn option(player: &mut char) {
+    match player {
+        'X' => {
+            println!("Passes {}", player);
+            *player = 'Y';
+        }
+        'Y' => {
+            println!("Passes {}", player);
+            *player = 'X';
+        },
+        _ => println!("Jpt")
+    }
+}
+
+fn take_input_from_user() -> Result<u8, Box<dyn std::error::Error>>{
+    println!("Please give input: ");
+    let mut input = String::new();
+    io::stdin().read_line(&mut  input).expect("Failed to get input data");
+    let num_inp = input.trim().parse::<u8>()?;
+
+    println!("User had type: {}", num_inp);
+    num_inp
+}
+
 pub fn init() {
     let mut board = vec![['_'; 3]; 3];
     let mut count = 1;
     let mut player_turn = 'X';
     board[0][0] = player_turn;
     board_display(&board);
-    
 
-    println!("Logging player: {:?}", player_turn);
-
+    loop {
+        option(&mut player_turn);
+        take_input_from_user();
+    }
+   
 }
