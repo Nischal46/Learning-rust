9f923d5237220146d923a823c07158c64938d001
Nischal Baniya <baniyanisal12@gmail.com>
2026-09-09 14:57:07 -0400

    simple rust array allocation and its limitation about compile time check

--- src/exercises/arrayquestions.rs
+++ src/exercises/arrayquestions.rs
@@ -1,47 +1,13 @@
-#[derive(Debug)]
-struct Accessories<'a> {
-    title: &'a str,
-    price: &'a str,
-    brand: &'a str,
-}
-
-#[derive(Debug)]
-struct ArrayContainer<'a> {
-    data: [Option<Accessories<'a>>; 5],
-}
-
-impl<'a> ArrayContainer<'a> {
-    fn init() -> Self {
-        // Self {
-        //     data: [Some(Accessories {
-        //         title: "Lovely laptop",
-        //         price: "68000",
-        //         brand: "Dell",
-        //     }); 5],
-        // }
-        Self {
-            data: std::array::from_fn(|_| None),
-        }
-    }
-
-    fn add_data_in_specific_index(&mut self) {
-        for (ind, it) in self.data.iter_mut().enumerate() {
-            if ind == 2 {
-                *it = Some(Accessories {
-                    title: "Lovely my laptop",
-                    price: "68000",
-                    brand: "DELL",
-                })
-            }
-        }
-    }
-}
-
 pub fn init() {
-    println!("This revision would contain array related solutions");
+    println!("This is rust concept for the array manipulation");
 
-    let mut array_container = ArrayContainer::init();
-    println!("Logging of the array container:");
-    println!("{:?}", array_container);
-    array_container.add_data_in_specific_index();
+    //this is check in compile time
+    let mut arr = ["hello", "hi"];
+    println!("{}", arr[0]);
+
+    arr[1] = "nischal";
+    println!("{:#?}", arr);
+
+    arr[2] = "ban";
+    println!(":?", arr);
 }
