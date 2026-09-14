ca16864c84d039576b830637e205f3ee7517a38c
Nischal Baniya <baniyanisal12@gmail.com>
2026-09-14 11:25:55 -0400

    looping and measuring time diff

--- src/exercises/arrayquestions.rs
+++ src/exercises/arrayquestions.rs
@@ -1,10 +1,12 @@
+use std::time::Instant;
+
 struct ArrayContainer {
-    data: [Option<i32>; 5]
+    data: [Option<i32>; 10]
 }
 
 impl ArrayContainer {
     fn new() -> Self {
-        Self { data: [None; 5] }
+        Self { data: [None; 10] }
     }
 
     fn extract_present_values_in_array(&self) -> Vec<i32> {
@@ -18,9 +20,19 @@ pub fn init() {
 
     let mut arr_container_declared = ArrayContainer::new();
     arr_container_declared.data[2] = Some(10);
+    // arr_container_declared.data[9998] = Some(23);
     println!("{:?}", arr_container_declared.data);
     println!("fetching of the arr index 3: {:?}", arr_container_declared.data[2].unwrap());
+    // println!("fetching of the arr index 9999: {:?}", arr_container_declared.data[9998].unwrap());
 
     let present_values = arr_container_declared.extract_present_values_in_array();
     println!("Present values in the array: {:?}", present_values);
+
+    let start = Instant::now();
+
+    for i in 0i64..1000000000 {
+    }
+    
+    let end = Instant::now();
+    println!("Time taken: {:?}", end - start);
 }
