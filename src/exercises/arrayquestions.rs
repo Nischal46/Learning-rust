38f0c6a6f41482e496cc9c0ff405891291402ca3
Nischal Baniya <baniyanisal12@gmail.com>
2026-09-16 16:12:45 -0400

    excersing with some array cool methods and also other left remaining

--- src/exercises/arrayquestions.rs
+++ src/exercises/arrayquestions.rs
@@ -1,38 +1,32 @@
-use std::time::Instant;
-
-struct ArrayContainer {
-    data: [Option<i32>; 10]
-}
-
-impl ArrayContainer {
-    fn new() -> Self {
-        Self { data: [None; 10] }
-    }
-
-    fn extract_present_values_in_array(&self) -> Vec<i32> {
-        self.data.iter().filter_map(|&x| x).collect()
-    }
-}
+// there are several ways to write array
 
 pub fn init() {
-    let arr = [1, 4, 3];
-    println!("{:?}", arr);
+    println!("------ | array concept | -------");
+    let mut arr_by_invoking: [u32; 6];
 
-    let mut arr_container_declared = ArrayContainer::new();
-    arr_container_declared.data[2] = Some(10);
-    // arr_container_declared.data[9998] = Some(23);
-    println!("{:?}", arr_container_declared.data);
-    println!("fetching of the arr index 3: {:?}", arr_container_declared.data[2].unwrap());
-    // println!("fetching of the arr index 9999: {:?}", arr_container_declared.data[9998].unwrap());
+    arr_by_invoking = [12, 43, 54, 25, 36, 87];
 
-    let present_values = arr_container_declared.extract_present_values_in_array();
-    println!("Present values in the array: {:?}", present_values);
+    arr_by_invoking[2] = 56;
 
-    let start = Instant::now();
+    //sorting method
 
-    for i in 0i64..1000000000 {
-    }
-    
-    let end = Instant::now();
-    println!("Time taken: {:?}", end - start);
+    arr_by_invoking.sort();
+
+    println!("Logging of the arr: {:?}", arr_by_invoking);
+
+    //slices method
+
+    let contain = arr_by_invoking.contains(&3);
+
+    println!("At last after using of the slices: {}", contain);
+
+    //first and last method
+
+    let first = arr_by_invoking.first();
+    let last = arr_by_invoking.last();
+
+    println!(
+        "displaying of the first and the last: {:?} , {:?}",
+        first, last
+    );
 }
